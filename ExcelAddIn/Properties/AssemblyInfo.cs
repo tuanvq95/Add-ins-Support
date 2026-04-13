7503499c06a0fcc94d1adf43bf6ba0a2d355cf55
Vũ Quốc Tuấn <tuan-vq@fujinet.net>
2026-04-13 18:42:42 +0700

    add src

---
+++ ExcelAddIn/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ExcelAddIn")]
+[assembly: AssemblyDescription("VSTO Excel Add-in hỗ trợ Excel 2013–2019: Auto Hyperlink, Đổi Tên Sheet, Nhúng Macro VBA")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExcelAddIn")]
+[assembly: AssemblyCopyright("Copyright © 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
